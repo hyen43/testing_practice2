bb2057b16589ae4b38c075899101d60672124c61
Nahyun Lee <lnhyen43@gmail.com>
2023-12-10 19:29:40 +0900

    TEST: form 컴포넌트 관련 initial  test case 추가

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,37 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+test("initial condition", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /I agree to Terms and Conditions/i,
+  });
+
+  const confirmButton = screen.getAllByRole("button", {
+    name: "Confirm order",
+  });
+
+  // 체크박스가 default로 체크가 되어있지 않은 상태인지 확인
+  expect(checkbox).not.toBeChecked();
+  // 체크박스가 체크되어있지 않았을 때, 버튼이 비활성화 상태인지 확인
+  expect(confirmButton).toBeDisabled();
+});
+
+test("체크박스의 체크여부에 따라 버튼 활성화 여부 확인 테스트", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /I agree to Terms and Conditions/i,
+  });
+
+  const confirmButton = screen.getAllByRole("button", {
+    name: "Confirm order",
+  });
+
+  // 체크박스를 체크하면 버튼 활성화 여부 확인
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(confirmButton).toBeEnabled();
+});
